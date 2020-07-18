356d6f0c8c75ac043269cb88cc166f7e9e6e8c84
Olga Belousova <volha.belausava@gmail.com>
2020-07-18 13:57:31 +0300

    DB connection created

---
+++ Exception/FileNotFoundException.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Exception;
+
+class FileNotFoundException extends \Exception
+{
+
+}
--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
+include __DIR__ . '/Component/Db.php';
+
+try {
+    $db = new \App\Component\Db();
+} catch (\App\Exception\FileNotFoundException $error) {
+    echo $error->getMessage();
+} catch (\Exception $error) {
+    $error->getMessage();
+}
+
 echo "Welcome to Front Controller";
